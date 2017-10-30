012594230e404c7be9aa605e152af769884f813b
frainfreeze <kucar.tomislav@gmail.com>
2017-10-30 21:04:21 +0100

    v4

---
+++ university/10004-prog/labs/v4/zadatak10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<ctime>
+using namespace std;
+
+/*
+Zadatak 10. Napravite Loto 7 / 45 aplikaciju.
+U bubnju se nalazi 45 brojeva. Korisnik treba odigrati 7 brojeva.
+Nakon odigranih brojeva prikazuju se izvuceni i odigrani brojevi, 
+broj pogodaka te pogodeni brojevi. Svi brojevi se prikazuju sortirano. 
+U aplikaciji treba osigurati da korisnik ne moze vise puta odigrati isti broj.
+*/
+
+int main()
+{
+	
+	return 0;
+}
+
+
