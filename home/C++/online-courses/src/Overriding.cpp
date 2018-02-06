ec29fdda2a07a1627401a92a8a8b4ef52096e8af
frainfreeze <kucar.tomislav@gmail.com>
2018-02-07 00:40:29 +0100

    udemy - basics

---
+++ home/C++/online-courses/src/Overriding.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	void run() {
+		cout << "Animal running" << endl;
+	}
+
+	void speak() {
+		cout << "Grrrr" << endl;
+	}
+};
+
+class Cat: public Animal {
+public:
+	void speak() {
+		cout << "Meeeouw" << endl;
+	}
+};
+
+
+int main() {
+	Animal animal;
+	animal.run();
+	animal.speak();
+
+	Cat cat;
+	cat.run();
+	cat.speak();
+
+	return 0;
+}
