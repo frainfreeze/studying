1f0b0dc49a8a3d6268de00f9c4697ef4e2b62c37
frainfreeze <kucar.tomislav@gmail.com>
2018-03-02 23:11:21 +0100

    fix sheet size, make print more sane

--- cpp/other/cmdexclpp/main.cpp
+++ cpp/other/cmdexclpp/main.cpp
@@ -1,4 +1,5 @@
 // biggest todo: stop crossing 80 cols!
+// keep me single file, no classes and templates till ver 1.0 ?
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,6 +7,7 @@
 #include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 // globals
@@ -18,10 +20,10 @@ string getFileContents(ifstream& File);
 
 auto start_time = chrono::high_resolution_clock::now();
 
-struct Cell { //17x8=133
-    int cell_pos;
-    string data; // actuall data
-    string cell; // to be displayed, 8 chars
+struct Cell { //17x8
+    int cell_pos; // position
+    string data; // actual data
+    string cell; // data to be displayed, 8 chars
 };
 
 void clearScreen() {
@@ -108,7 +110,7 @@ int menu(vector<Cell>& sheet){
 int main() {
     vector<Cell> sheet;
     // fill sheet with nothing
-    for(int i = 0;i<134;i++){
+    for(int i = 0;i<=(8*17);i++){
         Cell temp;
         temp.cell_pos = i;
         temp.data = "0";
@@ -163,25 +165,18 @@ int screen(vector<Cell>& sheet, int status, bool changes){
     cout << line_thin << endl;
     cout << "Status: " << statusTxt << " | Current editing time: " << clockx() << " seconds | " << changesTxt << endl;
     cout << line_double_thin << endl;
-    cout << "   " << "|" << "   a    " << "|" << "   b    " << "|" << "   c    " << "|" << "   d    " << "|" << "   e    " << "|" << "   f    " << "|" << "    g   " << "|" << "    h   " << "|"<<'\n';
+    printf("%s|%s|%s|%s|%s|%s|%s|%s|%s|\n","   ","   a    ","   b    ","   c    ","   d    ","   e    ","   f    ","    g   " ,"    h   ");
     cout << "---+--------+--------+--------+--------+--------+--------+--------+--------+\n";
-    cout << "1  " << "|" << sheet[0].cell << "|" << sheet[1].cell << "|" << sheet[2].cell << "|" << sheet[3].cell << "|" << sheet[4].cell << "|" << sheet[5].cell << "|" << sheet[6].cell << "|" << sheet[7].cell << "|" <<'\n';
-    cout << "2  " << "|" << sheet[8].cell << "|" << sheet[9].cell << "|" << sheet[10].cell << "|" << sheet[11].cell << "|" << sheet[12].cell << "|" << sheet[13].cell << "|" << sheet[14].cell << "|" << sheet[15].cell << "|" <<'\n';
-    cout << "3  " << "|" << sheet[16].cell << "|" << sheet[17].cell << "|" << sheet[18].cell << "|" << sheet[19].cell << "|" << sheet[20].cell << "|" << sheet[21].cell << "|" << sheet[22].cell << "|" << sheet[23].cell << "|" <<'\n';
-    cout << "4  " << "|" << sheet[24].cell << "|" << sheet[25].cell << "|" << sheet[26].cell << "|" << sheet[27].cell << "|" << sheet[28].cell << "|" << sheet[29].cell << "|" << sheet[30].cell << "|" << sheet[31].cell << "|" <<'\n';
-    cout << "5  " << "|" << sheet[32].cell << "|" << sheet[33].cell << "|" << sheet[34].cell << "|" << sheet[35].cell << "|" << sheet[36].cell << "|" << sheet[37].cell << "|" << sheet[38].cell << "|" << sheet[39].cell << "|" <<'\n';
-    cout << "6  " << "|" << sheet[40].cell << "|" << sheet[41].cell << "|" << sheet[42].cell << "|" << sheet[43].cell << "|" << sheet[44].cell << "|" << sheet[45].cell << "|" << sheet[46].cell << "|" << sheet[47].cell << "|" <<'\n';
-    cout << "7  " << "|" << sheet[48].cell << "|" << sheet[49].cell << "|" << sheet[50].cell << "|" << sheet[51].cell << "|" << sheet[52].cell << "|" << sheet[53].cell << "|" << sheet[54].cell << "|" << sheet[55].cell << "|" <<'\n';
-    cout << "8  " << "|" << sheet[56].cell << "|" << sheet[57].cell << "|" << sheet[58].cell << "|" << sheet[59].cell << "|" << sheet[60].cell << "|" << sheet[61].cell << "|" << sheet[62].cell << "|" << sheet[63].cell << "|" <<'\n';
-    cout << "9  " << "|" << sheet[64].cell << "|" << sheet[65].cell << "|" << sheet[66].cell << "|" << sheet[67].cell << "|" << sheet[68].cell << "|" << sheet[69].cell << "|" << sheet[70].cell << "|" << sheet[71].cell << "|" <<'\n';
-    cout << "10 " << "|" << sheet[70].cell << "|" << sheet[71].cell << "|" << sheet[72].cell << "|" << sheet[73].cell << "|" << sheet[74].cell << "|" << sheet[75].cell << "|" << sheet[76].cell << "|" << sheet[77].cell << "|" <<'\n';
-    cout << "11 " << "|" << sheet[78].cell << "|" << sheet[79].cell << "|" << sheet[80].cell << "|" << sheet[81].cell << "|" << sheet[82].cell << "|" << sheet[83].cell << "|" << sheet[84].cell << "|" << sheet[85].cell << "|" <<'\n';
-    cout << "12 " << "|" << sheet[86].cell << "|" << sheet[87].cell << "|" << sheet[88].cell << "|" << sheet[89].cell << "|" << sheet[90].cell << "|" << sheet[01].cell << "|" << sheet[92].cell << "|" << sheet[93].cell << "|" <<'\n';
-    cout << "13 " << "|" << sheet[94].cell << "|" << sheet[95].cell << "|" << sheet[96].cell << "|" << sheet[97].cell << "|" << sheet[98].cell << "|" << sheet[99].cell << "|" << sheet[100].cell << "|" << sheet[101].cell << "|" <<'\n';
-    cout << "14 " << "|" << sheet[102].cell << "|" << sheet[103].cell << "|" << sheet[104].cell << "|" << sheet[105].cell << "|" << sheet[106].cell << "|" << sheet[107].cell << "|" << sheet[108].cell << "|" << sheet[109].cell << "|" <<'\n';
-    cout << "15 " << "|" << sheet[110].cell << "|" << sheet[111].cell << "|" << sheet[112].cell << "|" << sheet[113].cell << "|" << sheet[114].cell << "|" << sheet[115].cell << "|" << sheet[116].cell << "|" << sheet[117].cell << "|" <<'\n';
-    cout << "16 " << "|" << sheet[118].cell << "|" << sheet[119].cell << "|" << sheet[120].cell << "|" << sheet[121].cell << "|" << sheet[122].cell << "|" << sheet[123].cell << "|" << sheet[124].cell << "|" << sheet[125].cell << "|" <<'\n';
-    cout << "17 " << "|" << sheet[126].cell << "|" << sheet[127].cell << "|" << sheet[128].cell << "|" << sheet[129].cell << "|" << sheet[130].cell << "|" << sheet[131].cell << "|" << sheet[132].cell << "|" << sheet[133].cell << "|" <<'\n';
+    int tempcounter = 0;
+    for (int i=0; i<17; i++){
+        for(int j=0; j<18; j++){
+            if (j == 0 && i < 9) cout << "  " << i+1 ;
+            if (j == 0 && i >= 9) cout << " " << i+1 ;
+            if (j%2== 0 && j!=0) cout << sheet[++tempcounter].cell;
+            if (j%2!= 0 && j!=0)cout << "|";
+        }
+        cout << endl;
+    }
     cout << line_bold_up << endl;
     cout << "Enter command:" << endl;
     return 0;
@@ -196,7 +191,7 @@ void clearx(vector<Cell>& sheet){
     cout << "\n\n\n\n\n\n\n\n\n\t\t\t!!! WARNING !!!!\n\tAre you absolutely sure you want to empty whole sheet?\n\tType 'yes' to proceed, anything else to cancel: ";
     getline(cin,temp);
     if(temp=="yes"){
-        for(int i = 0;i<134;i++){
+        for(int i = 0;i<(8*17);i++){
             sheet[i].cell_pos = i;
             sheet[i].data = "0";
             sheet[i].cell = "        ";
