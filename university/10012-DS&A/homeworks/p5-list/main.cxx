0e952feb6fe9a3498effcf85058281753be99e84
frainfreeze <kucar.tomislav@gmail.com>
2018-04-04 22:36:25 +0200

    double linked list

---
+++ university/10012-DS&A/homeworks/p5-list/main.cxx
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+/*	implementacija dvostruko povezane liste, podrzava:
+		o Izradu prazne liste
+		o Ubacivanje na kraj liste*/
+
+struct node {
+    int data;
+    node *next, *prev;
+};
+
+class list {
+public:
+    list();
+    void push_back(int num);
+    node *begin();
+    node *end(); // actually last element :p
+
+private:
+    node *head, *tail;
+};
+
+list::list()
+{
+    head = nullptr;
+    tail = nullptr;
+}
+
+void list::push_back(int num) {
+    node * tmp;
+    tmp = new node;
+    tmp->data = num;
+    tmp->next = nullptr;
+    tmp->prev = tail;
+
+    if (nullptr == head) {
+        head = tmp;
+        tail = tmp;
+    } else {
+        tail->next = tmp;
+        tail = tmp;
+    }
+}
+
+node *list::begin() {
+    return head;
+}
+
+node *list::end() {
+    return tail;
+}
+
+int main() {
+    list numbers;
+    for (int i = 0; i < 10; ++i) {
+        numbers.push_back(i+1);
+    }
+
+    std::cout << "list: ";
+
+    node *temp;
+    temp = numbers.begin();
+    while(temp!= nullptr) {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    std::cout << "\nreverse list: ";
+    temp = numbers.end();
+    while(temp!=nullptr) {
+        std::cout << temp->data << " ";
+        temp = temp->prev;
+    }
+
+	std::cout << std::endl;
+	return 0;
+}
