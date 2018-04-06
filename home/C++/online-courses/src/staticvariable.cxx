61a374022daacff59035e06a9d8bc2c2e52284c8
frainfreeze <kucar.tomislav@gmail.com>
2018-04-06 11:54:17 +0200

    virtual and static

---
+++ home/C++/online-courses/src/staticvariable.cxx
@@ -0,0 +1,20 @@
+//Static Variables:
+#include<iostream>
+
+using namespace std;
+
+int returnValue()
+{
+    static int i = 0;
+    i++;
+    return i;
+}
+
+
+int main()
+{
+    cout<<"\ni = "<<returnValue();
+    cout<<"\ni = "<<returnValue();
+    cout<<"\ni = "<<returnValue();
+    return 0;
+}
