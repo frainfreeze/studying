e2701dc97bb19673a40f9103f4ac56cabdbad281
frainfreeze <kucar.tomislav@gmail.com>
2018-04-07 22:42:56 +0200

    templates

---
+++ home/C++/online-courses/src/templates.cxx
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// generic function
+template <typename T>
+T findSmaller(T input1, T input2)
+{
+    if(input1<input2)
+        return input1;
+    else
+        return input2;
+}
+
+//tells the compiler that this is a function-template
+template<class TYPE> 
+// TYPE is template type parameter
+void PrintTwice(TYPE data)
+{
+    cout<<"Twice: " << data * 2 << endl;
+}
+
+int main()
+{
+    int a = 54; 
+    int b = 89;
+    float f1 = 7.8;
+    float f2 = 9.1;
+    char c1 = 'f';
+    char c2 = 'h';
+    string s1 = "Hello";
+    string s2 = "Bots are fun";
+    
+    cout << "\nIntegers compared: " << findSmaller(a,b);
+    cout << "\nFloats compared: " << findSmaller(f1,f2);
+    cout << "\nChars compared: " << findSmaller(c1,c2);
+    cout << "\nStrings compared: " << findSmaller(s1,s2);   
+    
+    cout << "\n";
+    PrintTwice(124);
+    PrintTwice(3.25);
+    
+    cout << "\n";
+    return 0;
+}
