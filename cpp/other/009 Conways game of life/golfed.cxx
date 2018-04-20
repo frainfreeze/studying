bb11cc55599eec6953d967cc4b7ef6c5f52e83d4
frainfreeze <kucar.tomislav@gmail.com>
2018-04-20 10:18:47 +0200

    golfed

--- cpp/other/009 Conways game of life/golfed.cxx
+++ cpp/other/009 Conways game of life/golfed.cxx
@@ -1,27 +1,6 @@
-%:include <iostream>
-%:include <bitset>
-%:include <ctime>
-%:include <chrono>
-%:include <thread>
-std::bitset<800> field;
-int main() <%
-	srand(static_cast<unsigned int>(time(nullptr)));
-	for (unsigned int i = 0; i < field.size(); ++i) if (!(rand() % 4)) field[i] = true;
-	loop:
-		std::this_thread::sleep_for(std::chrono::milliseconds(250));
-		std::bitset<800> nxtField;
-		for (struct {int i, neighbours;} a={0,0}; a.i <  field.size(); ++a.i) <% 
-			!(a.i % 40) ? std::cout << "\n" : field[a.i] ? std::cout << "#" : std::cout << " ";
-			a.neighbours += (a.i > 40  && field[a.i - 41]);
-			a.neighbours += (a.i > 39  && field[a.i - 40]);
-			a.neighbours += (a.i > 38  && field[a.i - 39]);
-			a.neighbours += (a.i > 0   && field[a.i - 1]);
-			a.neighbours += (a.i < 799 && field[a.i + 1]);
-			a.neighbours += (a.i < 761 && field[a.i + 39]);
-			a.neighbours += (a.i < 760 && field[a.i + 40]);
-			a.neighbours += (a.i < 759 && field[a.i + 41]);
-			a.neighbours < 2 || a.neighbours > 3 ? nxtField[a.i] = false : (a.neighbours == 3) || (field[a.i] == 1 && a.neighbours == 2) ?  nxtField[a.i] = true : true;
-			a.neighbours=0;
-		%>  field = nxtField;
-	goto loop;
-%>
+%:include<iostream>
+%:include<bitset>
+%:include<ctime>
+%:include<chrono>
+%:include<thread>
+int main(){std::bitset<800>f;srand(static_cast<unsigned int>(time(nullptr)));for(unsigned int i=0;i<f.size();++i)if(!(rand()%4))f[i]=true;for(;;){std::this_thread::sleep_for(std::chrono::milliseconds(250));std::bitset<800>nf;for(struct{int i,n;}a={0,0};a.i<f.size();++a.i)<%!(a.i%40)? std::cout<<"\n" : f[a.i] ? std::cout<<"#" : std::cout<<" ";a.n+=((a.i>40&&f[a.i-41])+(a.i>39&&f[a.i-40])+(a.i>38&&f[a.i-39])+(a.i>0&&f[a.i-1])+(a.i<799&&f[a.i+1])+(a.i<761&&f[a.i+39])+(a.i<760&&f[a.i+40])+(a.i<759&&f[a.i+41]));a.n<2||a.n>3 ? nf[a.i]=false :(a.n==3)||(f[a.i]==1&&a.n==2)? nf[a.i]=true : true;a.n=0;%>f=nf;}}
