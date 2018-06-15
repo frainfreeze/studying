01b6bc10854c1c5f54f45eaad22ddf33c0d231b0
frainfreeze <kucar.tomislav@gmail.com>
2018-06-15 22:10:38 +0200

    Update Source.cpp

--- Zafatak2/Source.cpp
+++ Zafatak2/Source.cpp
@@ -2,13 +2,11 @@
 #include <chrono>
 #include <thread>
 
-bool end = false;
-int steps = 0;
 char field[20][40];
 struct point { int x = 0, y = 0; } start, dest, current, goal;
 
 int main() { //sramim se.
-	int q, e, r, t;
+	int q, e, r, t, steps;
 	std::cout << "Unesite a.x, a.y, b.x, b.y s razmacima izmedu brojeva (a 0-19, b 0-39): ";
 	std::cin >> q >> e >> r >> t; //sanitize input??
 	start = { q,e };
@@ -16,15 +14,13 @@ int main() { //sramim se.
 	current = start;
 	field[5][1] = '*'; field[5][2] = '*'; field[5][3] = '*'; field[5][4] = '*';
 
-	while (!end) {
+	for(;;) {
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		system("CLS");
-		if (current.x == goal.x && current.y == goal.y) end = true;
-		else { // path finding
-			if (current.x != goal.x && field[current.x + 1][current.y] != '*')  current.x++;
-			else if (current.y != goal.y && field[current.x][current.y + 1] != '*') current.y++;
-			steps++;
-		}
+		if (current.x == goal.x && current.y == goal.y) break;
+		if (current.x != goal.x && field[current.x + 1][current.y] != '*')  current.x++;
+		else if (current.y != goal.y && field[current.x][current.y + 1] != '*') current.y++;
+		steps++;
 
 		for (int i = 0; i < 20; ++i) {//fill array
 			for (int j = 0; j < 40; ++j) {
