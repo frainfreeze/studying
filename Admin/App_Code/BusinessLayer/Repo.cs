117965880df7ef5e35d86e024f464d251356b8e6
‌‌ frainfreeze <kucartomislav+github@gmail.com>
2019-06-18 20:08:39 +0200

    Refactor: seperate business layer

--- Admin/App_Code/BusinessLayer/Repo.cs
+++ Admin/App_Code/BusinessLayer/Repo.cs
@@ -49,5 +49,23 @@ namespace Admin.App_Code.BusinessLayer
             return sb.ToString();
         }
 
+        public int ValidateUser(object usr, object pwd)
+        {
+            int userId = 0;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                using (SqlCommand cmd = new SqlCommand("Validate_User"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Username", usr);
+                    cmd.Parameters.AddWithValue("@Password", pwd);
+                    cmd.Connection = con;
+                    con.Open();
+                    userId = Convert.ToInt32(cmd.ExecuteScalar());
+                    con.Close();
+                }
+                return userId;
+            }
+        }
     }
 }
--- Admin/Login.aspx.cs
+++ Admin/Login.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Admin.App_Code.BusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -15,32 +16,18 @@ namespace Admin
     {
         protected void ValidateUser(object sender, EventArgs e)
         {
-            int userId = 0;
-            string constr = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
+            Repo repo = new Repo();
+            switch (repo.ValidateUser(Login1.UserName, Login1.Password))
             {
-                using (SqlCommand cmd = new SqlCommand("Validate_User"))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@Username", Login1.UserName);
-                    cmd.Parameters.AddWithValue("@Password", Login1.Password);
-                    cmd.Connection = con;
-                    con.Open();
-                    userId = Convert.ToInt32(cmd.ExecuteScalar());
-                    con.Close();
-                }
-                switch (userId)
-                {
-                    case -1:
-                        Login1.FailureText = "Username and/or password is incorrect.";
-                        break;
-                    case -2:
-                        Login1.FailureText = "Account is not admin.";
-                        break;
-                    default:
-                        FormsAuthentication.RedirectFromLoginPage(Login1.UserName, Login1.RememberMeSet);
-                        break;
-                }
+                case -1:
+                    Login1.FailureText = "Username and/or password is incorrect.";
+                    break;
+                case -2:
+                    Login1.FailureText = "Account is not admin.";
+                    break;
+                default:
+                    FormsAuthentication.RedirectFromLoginPage(Login1.UserName, Login1.RememberMeSet);
+                    break;
             }
         }
     }
