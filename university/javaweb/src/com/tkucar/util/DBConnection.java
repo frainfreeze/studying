8ba878d95e58e763baf628a368315b17a8e35ce9
frainfreeze <kucar.tomislav@gmail.com>
2020-06-07 19:35:49 +0200

    update spo & java

--- university/javaweb/src/com/tkucar/util/DBConnection.java
+++ university/javaweb/src/com/tkucar/util/DBConnection.java
@@ -8,20 +8,9 @@ public class DBConnection {
     final String url = "jdbc:sqlite:/home/frain/git/studying/university/javaweb/redrivers.db";
 
     public Connection getConn() {
-
         try {
-//            Connection con;
-//            //loading drivers for mysql
-//            Class.forName("com.mysql.jdbc.Driver");
-//
-//            //creating connection with the database
-//            con=DriverManager.getConnection
-//                    ("jdbc:mysql://localhost:3306/user","root","");
-            Connection conn;
             Class.forName("org.sqlite.JDBC");
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-
+            Connection conn = DriverManager.getConnection(url);
             return conn;
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
